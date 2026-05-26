9be9cc67363cf990ebc64aa4912aee3c2c768123
Vitorinha-developeer <maria.liberal@academico.ifpb.edu.br>
2026-05-26 14:09:00 -0300

    Enhance editarEvento to manage event registrations
    Added logic to update event registrations when editing an event.

--- IFPB/controller/EventoController.java
+++ IFPB/controller/EventoController.java
@@ -1,9 +1,11 @@
 package Even.IFPB.controller;
 
 import Even.IFPB.model.Evento;
+import Even.IFPB.model.Inscrição;
 import Even.IFPB.model.Usuário;
 
 import Even.IFPB.repository.repositoryEvento;
+import Even.IFPB.repository.repositoryInscricao;
 import Even.IFPB.repository.repositoryUsuário;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +22,9 @@ public class EventoController {
 
     @Autowired
     private repositoryUsuário repositoryusuario;
+
+    @Autowired
+    private repositoryInscricao repositoryinscricao;
 
     // CADASTRAR
     @PostMapping("/cadastrarevento")
@@ -72,14 +77,14 @@ public class EventoController {
     public void editarEvento(@PathVariable Long id,
                              @RequestBody Evento eventoAtualizado){
 
-        if(repositoryevento.existsById(id)){
+        if(repositoryevento.existsById(id)) {
 
             Evento eventoExistente = repositoryevento.findById(id).orElseThrow();
 
             Long idUsuario = eventoAtualizado.getUsuario().getId_usuario();
 
             Usuário usuario = repositoryusuario.findById(idUsuario).orElseThrow(() ->
-                                    new RuntimeException("Usuário não encontrado"));
+                    new RuntimeException("Usuário não encontrado"));
 
             eventoExistente.setUsuario(usuario);
 
@@ -96,6 +101,23 @@ public class EventoController {
             eventoExistente.setTipo_evento(eventoAtualizado.getTipo_evento());
 
             eventoExistente.setQtde_vagas(eventoAtualizado.getQtde_vagas());
+
+            repositoryevento.save(eventoExistente);
+
+            for (Inscrição in : repositoryinscricao.findAll()) {
+
+                if (in.getEvento() != null
+                        && in.getEvento().getId_evento().equals(id)
+                        && in.getStatus() == Inscrição.Status.Pendente
+                        && eventoExistente.getQtde_vagas() > 0) {
+
+                    in.setStatus(Inscrição.Status.Confirmada);
+
+                    eventoExistente.setQtde_vagas(eventoExistente.getQtde_vagas() - 1);
+
+                    repositoryinscricao.save(in);
+                }
+            }
 
             repositoryevento.save(eventoExistente);
         }
